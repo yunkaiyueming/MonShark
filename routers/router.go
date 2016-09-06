211f88d9fbe45fc317ccc3ab32d16eccf5a1d813
can_pc <1835373375@qq.com>
2016-09-06 20:08:02 +0800

    添加注册路由

--- routers/router.go
+++ routers/router.go
@@ -11,4 +11,5 @@ func init() {
 
 	beego.Router("home/", &controllers.HomeController{}, "GET:Index")
 	beego.Router("home/index", &controllers.HomeController{}, "GET:Index")
+	beego.Router("home/register", &controllers.HomeController{}, "*:Register") //如果这个地方用POST，会导致在控制器中用this.GetString()方法无法获取到数据
 }
