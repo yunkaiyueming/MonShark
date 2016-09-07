7a991422eb9885e1b1d0c7fc089ed91d55840171
can_pc <1835373375@qq.com>
2016-09-07 15:00:04 +0800

    添加登录和  注销 路由

--- routers/router.go
+++ routers/router.go
@@ -7,8 +7,10 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("home/", &controllers.HomeController{}, "GET:Index")
-	beego.Router("home/index", &controllers.HomeController{}, "GET:Index")
-	beego.Router("home/register", &controllers.HomeController{}, "*:Register") //如果这个地方用POST，会导致在控制器中用this.GetString()方法无法获取到数据
-	beego.Router("user/", &controllers.UserController{}, "GET:InsertTest")
+	beego.Router("home/", &controllers.HomeController{}, "*:Index")
+	beego.Router("home/index", &controllers.HomeController{}, "*:Index")
+	beego.Router("user/register", &controllers.UserController{}, "*:Register") //如果这个地方用POST，会导致在控制器中用this.GetString()方法无法获取到数据
+	beego.Router("user/doregister", &controllers.UserController{}, "*:DoRegister")
+	beego.Router("user/login", &controllers.UserController{}, "*:DoLogin")
+	beego.Router("user/logout", &controllers.UserController{}, "*:LogOut")
 }
