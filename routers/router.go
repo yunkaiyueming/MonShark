925f5e1363f67d3d6e29f2a9b1265a728e4e0a5f
hongcheng.xu <819664855@qq.com>
2016-09-07 13:14:30 +0800

    增加首页概览

--- routers/router.go
+++ routers/router.go
@@ -8,8 +8,11 @@ import (
 func init() {
 	beego.Router("/", &controllers.MainController{})
 
+	//数据管理模块
 	beego.Router("home/", &controllers.HomeController{}, "GET:Index")
 	beego.Router("home/index", &controllers.HomeController{}, "GET:Index")
+	beego.Router("home/ShowMgoData", &controllers.HomeController{}, "GET:ShowMgoData")
 
+	//用户模块
 	beego.Router("user/", &controllers.UserController{}, "GET:InsertTest")
 }
